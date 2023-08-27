3705b1186057047bd3c45a2d0d2281c3224c5d4f
Drewbi <drewalexander986@gmail.com>
2023-08-27 12:26:23 +0800

    update logs

--- src/worker.ts
+++ src/worker.ts
@@ -40,35 +40,34 @@ export class ScreenState {
         let session: any = { webSocket, id: this.sessions.length };
         this.sessions.push(session);
 
+        console.log('test1')
         webSocket.addEventListener('message', async msg => {
             try {
                 if (session.quit) {
                     webSocket.close(1011, "WebSocket broken.");
                     return;
                 }
-                    console.log(msg)
 
-                    let data = JSON.parse(msg.data as string);
+                console.log(msg.data)
 
-                    console.log(data)
-                    
-                    if (!data.end && (data.x === undefined || data.y === undefined || data.x < 0 || data.y < 0 || data.x >= 16 || data.y >= 16)) {
-                        webSocket.send(JSON.stringify({ error: "Invalid message" }));
-                        return;
-                    }
-                    
-                    const { x: xDat = null, y: yDat = null } = data
-                    
-                    if (data.end) {
-                        await this.store.delete(session.id);
-                    } else await this.store.put(session.id, { x: xDat, y: yDat });
-                    
-                    const dataList = await Promise.all(this.sessions.map(session => this.store.get(session.id)))
-                    
-                    let dataStr = JSON.stringify(dataList);
-                    this.broadcast(dataStr);                    
-                } catch (err: any) {
-                webSocket.send(JSON.stringify({ error: 'something went wrong' }));
+                let data = JSON.parse(msg.data as string);
+
+                if (!data.end && (data.x === undefined || data.y === undefined || data.x < 0 || data.y < 0 || data.x >= 16 || data.y >= 16)) {
+                    throw Error('Invalid message');
+                }
+
+                const { x: xDat = null, y: yDat = null } = data
+
+                if (data.end) {
+                    await this.store.delete(session.id);
+                } else await this.store.put(session.id, { x: xDat, y: yDat });
+
+                const dataList = await Promise.all(this.sessions.map(session => this.store.get(session.id)))
+
+                let dataStr = JSON.stringify(dataList);
+                this.broadcast(dataStr);
+            } catch (err: any) {
+                webSocket.send(JSON.stringify({ error: 'Something went wrong: ' + err.message }));
             }
         });
         let closeOrErrorHandler = () => {
@@ -102,6 +101,7 @@ export default {
     async fetch(request: Request, env: Env): Promise<Response> {
         const id = env.SCREEN_STATE.idFromName('global')
         const stub = env.SCREEN_STATE.get(id);
+        console.log('2')
         return await stub.fetch(request)
     }
 };
